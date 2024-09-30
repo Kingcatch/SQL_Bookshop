7501cf5fef8082f86540405fd895e83c57bc5094
Laura Pugsley <lpugsley@hotmail.co.uk>
2024-09-30 12:34:07 +0100

    query books table

--- db/04_select_books.sql
+++ db/04_select_books.sql
@@ -1,19 +1,26 @@
 \c my_bookshop;
 
+
 \echo '\n Here are all the books that we have in stock:\n'
+SELECT * FROM books
+WHERE quantity_in_stock > 0;
 
 
 
 \echo '\n Here are all the non-fiction books:\n'
-
+SELECT * FROM books
+WHERE is_fiction = FALSE;
 
 
 \echo '\n Here are all the books released in the 1900s:\n'
-
+SELECT * FROM books
+WHERE release_date
+BETWEEN '1900-01-01' AND '1999-12-31';
 
 
 \echo '\n Here are all the books with "The" in the title:\n'
-
+SELECT * FROM books
+WHERE title SIMILAR TO '% the %';
 
 
 \echo '\n Here are the books sorted in alphabetical order:\n'
