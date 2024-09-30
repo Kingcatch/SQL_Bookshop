0f596cd1e20f9b7bf77119aeca55a3b01cdf1f58
Nana <nanagalaxy385@gmail.com>
2024-09-30 11:42:00 +0100

    books table created and unpopulated

--- db/01_create_database.sql
+++ db/01_create_database.sql
@@ -4,3 +4,14 @@ DROP DATABASE IF EXISTS my_bookshop;
 CREATE DATABASE my_bookshop;
 
 \c my_bookshop
+
+CREATE TABLE books (
+    books_id SERIAL PRIMARY KEY,
+    title VARCHAR(40) NOT NULL,
+    price_in_pence INT,
+    quantity_in_stock INT,
+    release_date DATE,
+    is_fiction BOOLEAN 
+);
+
+SELECT * FROM books 
