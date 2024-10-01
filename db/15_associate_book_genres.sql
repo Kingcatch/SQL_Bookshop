306c01d8066ddf5e437d7ef02fb7dc966c963c78
Nana <nanagalaxy385@gmail.com>
2024-10-01 15:07:56 +0100

    task 15 complete

--- db/15_associate_book_genres.sql
+++ db/15_associate_book_genres.sql
@@ -1,1 +1,15 @@
 \c my_bookshop
+
+INSERT INTO Genres_of_books
+    (books_id, genre_id)
+VALUES
+    (1, 7),
+    (2, 2),
+    (3, 2),
+    (4, 8),
+    (5, 5),
+    (6, 5),
+    (7, 1),
+    (10, 8);
+
+ SELECT * FROM Genres_of_books;
