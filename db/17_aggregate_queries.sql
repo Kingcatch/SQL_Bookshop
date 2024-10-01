c3ac4fe4fa25f73a98fba0803fe9f5cedb9797ff
Laura Pugsley <lpugsley@hotmail.co.uk>
2024-10-01 16:26:20 +0100

    complete task 17

--- db/17_aggregate_queries.sql
+++ db/17_aggregate_queries.sql
@@ -2,6 +2,16 @@
 
 \echo '\n Here is the total number of books we have by each author:\n'
 
-
+SELECT books.author_name, COUNT(books_id) AS total_number_of_books
+FROM books
+GROUP BY author_name;
 
 \echo '\n Here is the average price for dystopian books:\n'
+
+
+SELECT genre, ROUND(AVG(price_in_pence),2) AS average_price
+FROM books
+JOIN genres_of_books on genres_of_books.books_id = books.books_id
+JOIN genres ON genres_of_books.genre_id = genres.genre_id
+WHERE genre = 'Dystopian'
+GROUP BY genre;
