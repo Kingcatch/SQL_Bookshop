888a437d794c9730fa14fc90cd4fb03069f60546
Nana <nanagalaxy385@gmail.com>
2024-10-01 14:54:02 +0100

    task 14 complete

--- db/14_create_books_genres_table.sql
+++ db/14_create_books_genres_table.sql
@@ -1,1 +1,10 @@
 \c my_bookshop
+
+CREATE TABLE Genres_of_books (
+      books_id INT REFERENCES books(books_id),
+      genre_id INT REFERENCES Genres(genre_id),
+      Genres_of_books SERIAL PRIMARY KEY
+);
+
+SELECT * FROM Genres_of_books;
+  
