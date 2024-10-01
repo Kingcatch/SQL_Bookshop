5ae93a6d4e327fc6caca9c54537ea7e1420073ce
Nana <nanagalaxy385@gmail.com>
2024-10-01 15:57:19 +0100

    task 16 complete

--- db/16_query_books_with_genres.sql
+++ db/16_query_books_with_genres.sql
@@ -2,6 +2,25 @@
 
 \echo '\n Here are the genres for Emma:\n'
 
+-- SELECT * FROM books
+-- JOIN genres_of_books on genres_of_books.books_id = books.books_id
+-- JOIN genres ON genres_of_books.genre_id = genres.genre_id;
 
+-- SELECT * FROM genres_of_books;
+
+-- SELECT genre_id FROM genres_of_books
+-- WHERE books_id = 4;
+
+SELECT genre FROM books
+JOIN genres_of_books on genres_of_books.books_id = books.books_id
+JOIN genres ON genres_of_books.genre_id = genres.genre_id
+WHERE title = 'Pride and Prejudice';
+-- SELECT * FROM genres_of_books;
 
 \echo '\n Here are all the dystopian books:\n'
+SELECT title FROM books
+JOIN genres_of_books on genres_of_books.books_id = books.books_id
+JOIN genres ON genres_of_books.genre_id = genres.genre_id
+WHERE genre = 'Dystopian';
+
+
