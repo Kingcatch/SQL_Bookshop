b66ff1d5f00f4fe5a52de0a5c71c985738f4491c
Nana <nanagalaxy385@gmail.com>
2024-10-01 12:25:21 +0100

    task 11 complete

--- db/11_select_books_with_author.sql
+++ db/11_select_books_with_author.sql
@@ -2,6 +2,14 @@
 
 \echo '\n Here is a list of book titles with the corresponding author:\n'
 
-
+SELECT books.title, books.author_name
+FROM books
+JOIN authors
+ON books.author_name = authors.author_name;
 
 \echo '\n Here is a list of authors without an associated book:\n'
+
+SELECT authors.author_name FROM authors
+LEFT OUTER JOIN books ON authors.author_name = books.author_name
+WHERE books.author_name is NULL;
+
