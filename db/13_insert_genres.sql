86b1c91782a60fd34b0216439d4e2b3f412b8f09
Nana <nanagalaxy385@gmail.com>
2024-10-01 12:36:21 +0100

    task 13 complete

--- db/13_insert_genres.sql
+++ db/13_insert_genres.sql
@@ -1,1 +1,15 @@
 \c my_bookshop
+
+INSERT INTO Genres
+    (genre)
+VALUES 
+    ('Science fiction'),
+    ('Children''s'),
+    ('Romance'),
+    ('Fantasy'),
+    ('Dystopian'),
+    ('Science'),
+    ('Adventure'),
+    ('Classics');
+
+SELECT * FROM Genres;
